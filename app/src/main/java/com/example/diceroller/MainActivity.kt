6fdd87eaf68411317803625cb8c31dd6f1fbba86
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2023-04-06 16:22:20 +0700

    dynamic random number

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -5,6 +5,10 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,17 +16,18 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton : Button = findViewById(R.id.button)
 
-        rollButton.setOnClickListener {
-//            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_LONG)
-//            toast.show()
-//            resultTextView.text = "6"
-            rollDice()
-        }
+        rollButton.setOnClickListener { rollDice() }
     }
 
+    /**
+     * Roll the dice and update the screen with the result.
+     */
     private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.Roll()
+
+        // Update screen with the result of dice roll
         val resultTextView : TextView = findViewById(R.id.textView)
         resultTextView.text = diceRoll.toString()
     }
